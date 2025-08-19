e74dda17f1f8d137120289339a5d2fef3561eaf4
RaiyanMFZ <sadgidmahfuz@gmail.com>
2025-08-19 15:48:16 +0200

    buttons erstellt

--- my-app/src/app/page.js
+++ my-app/src/app/page.js
@@ -11,6 +11,30 @@ export default function Page() {
         <div className="bg-black text-green-400 p-2 rounded mb-3 text-right overflow-hidden">
           {screen}
         </div>
+        <div className="grid grid-cols-4 gap-2">
+          <button className="bg-red-700 text-white p-2 rounded">AC</button>
+          <button className="bg-yellow-500 text-white p-2 rounded">/</button>
+          <button className="bg-yellow-500 text-white p-2 rounded">*</button>
+          <button className="bg-yellow-500 text-white p-2 rounded">C</button>
+
+          <button className="bg-gray-300 text-white p-2 rounded">7</button>
+          <button className="bg-gray-300 text-white p-2 rounded">8</button>
+          <button className="bg-gray-300 text-white p-2 rounded">9</button>
+          <button className="bg-yellow-500 text-white p-2 rounded">-</button>
+
+          <button className="bg-gray-300 text-white p-2 rounded">4</button>
+          <button className="bg-gray-300 text-white p-2 rounded">5</button>
+          <button className="bg-gray-300 text-white p-2 rounded">6</button>
+          <button className="bg-yellow-500 text-white p-2 rounded">+</button>
+
+          <button className="bg-gray-300 text-white p-2 rounded">1</button>
+          <button className="bg-gray-300 text-white p-2 rounded">2</button>
+          <button className="bg-gray-300 text-white p-2 rounded">3</button>
+          <button className="bg-yellow-500 text-white p-2 rounded">=</button>
+
+          <button className="bg-gray-300 text-white p-2 rounded">0</button>
+          <button className="bg-gray-300 text-white p-2 rounded">.</button>
+        </div>
       </div>
     </div>
   )
